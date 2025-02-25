a08b5f8d92c2e38e965effb2077b323417a2bcf9
Carlos Henrique Nunes <carloshnunes383@gmail.com>
2025-02-25 11:11:28 -0300

    :sparkles: Consumo do Backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
-import Footer from './components/footer/Footer';
-import Navbar from './components/navbar/Navbar';
-import Home from './pages/home/Home';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import DeletarCategoria from "./components/categorias/deletarcategorias/DeletarCategoria";
+import FormCategoria from "./components/categorias/formcategoria/FormCategoria";
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./pages/home/Home";
+import ListarCategorias from "./components/categorias/listacategorias/ListaCategorias";
 
 function App() {
+
   return (
     <>
-      <Navbar />
-      <Home />
-      <Footer />
+      <BrowserRouter>
+        <Navbar />
+        <div className="flex flex-col min-h-screen bg-gray-200 ">
+          <div className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/categorias" element={<ListarCategorias />} />
+              <Route path="/cadastrarcategoria" element={<FormCategoria />} />
+              <Route path="/editarcategoria/:id" element={<FormCategoria />} />
+              <Route path="/deletarcategoria/:id" element={<DeletarCategoria />} />
+            </Routes>
+          </div>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
 
-export default App;
+export default App
